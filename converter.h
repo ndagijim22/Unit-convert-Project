29fe0d5f251755f4344b82cef3de84b3b20b4453
ndagijim22 <58204775+ndagijim22@users.noreply.github.com>
2023-03-15 13:55:21 -0700

    Created a header for function prototypes

---
+++ converter.h
@@ -0,0 +1,8 @@
+/* This is a head file for with function declaration 
+These functions will be defined in converter.c*/
+
+void temperatureConverter();
+void massConverter();
+void currencyConverter();
+
+//global variable
